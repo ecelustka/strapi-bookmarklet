7741769bf12aa43864377a51e9ca3ee42df1d60c
Emil Čelustka <emil.celustka@gmail.com>
2020-09-05 22:17:28 +0200

    init repository

---
+++ strapi-bookmarklet.js
@@ -0,0 +1,42 @@
+javascript:(
+    function () {
+    // set basic variables
+    var components = document.querySelectorAll('.component_name');
+    var styleTag = document.createElement('style');
+    var overlayCss =  `.custom-overlay ~ label + div { 
+        height: 80px;
+        overflow: hidden;
+        position: relative
+      }
+      .custom-overlay ~ label + div::after {
+          display: block;
+          content: '';
+          width: 100%;
+          background: linear-gradient(0deg, rgba(255,255,255,1) 0%, rgba(255,255,255,0) 100%);
+          height: 100%;
+          position: absolute;
+          top: 0;
+          z-index: 1;
+        }
+        `;
+
+    components.forEach(function(component) {
+        // open component labels
+        component.style.width = 'auto';
+        component.style.paddingLeft = '39px';
+        component.classList.add('custom-overlay');
+
+        // add class to parent
+        var parent = component.parentElement;
+        parent.classList.add('custom-overlay');
+        
+        // add eventlistener to parent
+        parent.addEventListener('click', function() {
+            parent.classList.remove('custom-overlay');
+        }, false);
+    });
+
+    // add stylesheet for components overlay
+    styleTag.innerHTML = overlayCss;
+    document.body.appendChild(styleTag);
+})();
